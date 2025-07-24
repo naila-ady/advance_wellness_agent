2facd3c2c1c35cc37f5de2f6dfdf5dbb36008104
naila-ady <164941279+naila-ady@users.noreply.github.com>
2025-07-25 00:43:39 +0500

    .

--- all_agents/injurysupport_agent.py
+++ all_agents/injurysupport_agent.py
@@ -3,7 +3,10 @@ from tools import workout_recommender
 
 injury_support_agent = Agent(
     name="injury_support_agent",
-    instructions="You assist users with joint pain or injuries",
+    instructions=("You assist users with joint pain or injuries"
+                "Gives tips for rest, stretches, or when to see a doctor."
+                "For a minor ankle pain, do Rest, try Ice packs, Compression, Elevation."
+                "move to workout_recommender tool "),
     # handoff_description="Helps with joint pain, swelling, or physical injury concerns.",
     tools=[workout_recommender]
     
