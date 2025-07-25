9415965c2f9ed5fd6b031cc2bf09c41ce4bbe81b
naila-ady <164941279+naila-ady@users.noreply.github.com>
2025-07-25 22:01:52 +0500

    updated injury support

--- all_agents/injurysupport_agent.py
+++ all_agents/injurysupport_agent.py
@@ -3,10 +3,12 @@ from tools import workout_recommender
 
 injury_support_agent = Agent(
     name="injury_support_agent",
-    instructions=("You assist users with joint pain or injuries"
+    instructions=("You assist users with asking what kind of pain or injuries user have"
+                  "then ask them for how long user is having pain than "
                 "Gives tips for rest, stretches, or when to see a doctor."
+                "if user say i have accident and want medical treatment use direct LLM to answer"
                 "For a minor ankle pain, do Rest, try Ice packs, Compression, Elevation."
-                "move to workout_recommender tool "),
+                " or if need excercise use workout_recommender tool "),
     # handoff_description="Helps with joint pain, swelling, or physical injury concerns.",
     tools=[workout_recommender]
     
